add437d183729cff8659b25885f08d34bef5a855
Erik Da Silva Negreiros <101146615+eriknegreiros@users.noreply.github.com>
2022-09-08 14:53:42 -0300

    Update api.js

--- src/scripts/models/api.js
+++ src/scripts/models/api.js
@@ -20,7 +20,7 @@ export class Request {
                 localStorage.setItem('user_uuid', res.data.user_uuid)
                 Toast.create('Login Realizado com sucesso', "#364fbb")
                 setTimeout(() => {
-                    window.location.replace("../../../src/pages/dashboard.html")
+                    window.location.replace("./src/pages/dashboard.html")
                 }, 1000)
                 return res
             })
